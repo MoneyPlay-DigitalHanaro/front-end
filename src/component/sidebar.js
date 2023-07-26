c64ff9fa988747123da3c1987bcd048286643365
tusker4 <tusker4@naver.com>
2023-07-26 16:54:06 +0900

    Add : 티머니, 안동까투리체 글꼴 추가

--- src/component/sidebar.js
+++ src/component/sidebar.js
@@ -5,14 +5,14 @@ function SideBar() {
     <div className="sidebar">
       <ul class="nav sidebar-nav">
         <li class="sidebar-brand">
-          <a href="#">금전 놀이</a>
+          <a href="/">금전 놀이</a>
         </li>
         <li>
-          <a href="#">포인트 관리</a>
+          <a href="/admin">포인트 관리</a>
         </li>
 
         <li>
-          <a href="../../src/pages/admin/admin.js">학생 관리</a>
+          <a href="/admin">학생 관리</a>
         </li>
       </ul>
     </div>
