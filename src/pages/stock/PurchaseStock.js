c96e8d5c52d9157a1cb87136e906ce1056b8f8ec
tusker4 <tusker4@naver.com>
2023-08-21 16:14:28 +0900

    Feat: 매수매도 페이지 UI

--- src/pages/stock/PurchaseStock.js
+++ src/pages/stock/PurchaseStock.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
-
+import styles from '../../style/css/Stock.module.css';
+import plus from '../../image/Stock/plus.png';
+import minus from '../../image/Stock/minus.png';
 import React, { useState } from 'react';
 
 function PurchaseStock() {
@@ -23,31 +25,48 @@ function PurchaseStock() {
 
   return (
     <div>
-      <h1>주식 이름</h1>
-
-      <div
-        style={{
-          border: '1px solid black',
-          width: '300px',
-          height: '200px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          margin: '20px 0',
-        }}
-      >
-        {/* 이곳에 실제 주식 그래프를 표현하는 코드나 이미지를 넣어주세요. */}
-        <span>주식 그래프 (아무거나)</span>
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-end', padding: '25px' }}>
+        <div className={`${styles.title} mgr15`}>삼성전자</div>
+        <div className={`${styles.titleNumber} `}>005930</div>
       </div>
+      <div className={`${styles.stockContainer} `}>
+        <div
+          style={{
+            border: '1px solid black',
+            width: '300px',
+            height: '200px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: '20px 0',
+          }}
+        >
+          {/* 이곳에 실제 주식 그래프를 표현하는 코드나 이미지를 넣어주세요. */}
+          <span>주식 그래프 (아무거나)</span>
+        </div>
+        <div className={`${styles.title}`}>50,000원</div>
+        <div className={`${styles.difference} ${styles.difference.startsWith('+') ? styles.blue : styles.red}`}>
+          +100 (+0.2%)
+        </div>
 
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <button onClick={decreaseStock}> </button>
-        <span>{stockCount}</span> 주<button onClick={increaseStock}> </button>
-      </div>
+        <div style={{ display: 'flex', alignItems: 'center', marginTop: '50px' }}>
+          <button onClick={decreaseStock} style={{ border: '0px', backgroundColor: 'white' }}>
+            <img src={minus}></img> 
+          </button>
+          <span className={`${styles.title}`}>{stockCount}</span> 주
+          <button onClick={increaseStock} style={{ border: '0px', backgroundColor: 'white' }}>
+            <img src={plus}></img>{' '}
+          </button>
+        </div>
 
-      <div>
-        <button onClick={buyStock}>사기</button>
-        <button onClick={sellStock}>팔기</button>
+        <div style={{ marginTop: '30px' }}>
+          <button className={`${styles.stockButton}`} onClick={buyStock}>
+            사기
+          </button>
+          <button className={`${styles.stockButton}`} onClick={sellStock}>
+            팔기
+          </button>
+        </div>
       </div>
     </div>
   );
