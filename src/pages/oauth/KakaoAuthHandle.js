dffd3f1393061d39682b320f4877d279c3119b8a
minjae Park <cfctor9@gmail.com>
2023-08-22 17:21:26 +0900

    fix : 다시 올림

--- src/pages/oauth/KakaoAuthHandle.js
+++ src/pages/oauth/KakaoAuthHandle.js
@@ -16,7 +16,7 @@
 //           localStorage.setItem('token_body', JSON.stringify(res.data)); // Storing response data in JSON format
 //           window.location.href = "/";
 //         })
-      
+
 //       }
 //       kakaoLogin()
 //     }, [props.history])
@@ -28,46 +28,50 @@
 //   }
 //   export default KakaoAuthHandle
 
-
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const KakaoAuthHandle = (props) => {
-    const navigate = useNavigate(); // 프로그램적으로 라우트를 변경하기 위해 useNavigate 사용
+  const navigate = useNavigate(); // 프로그램적으로 라우트를 변경하기 위해 useNavigate 사용
 
-    useEffect(() => {
-        let code = new URL(window.location.href).searchParams.get('code');
-        
-        const kakaoLogin = async () => {
-            try {
-                const res = await axios.get(`http://localhost:8080/api/login/oauth2/code/kakao?code=${code}`);
-                const authToken = res.data.authorization;
-                console.log(res);
+  useEffect(() => {
+    let code = new URL(window.location.href).searchParams.get("code");
 
-                localStorage.setItem('Authorization', authToken);
-                localStorage.setItem('token_body', JSON.stringify(res.data));
+    const kakaoLogin = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:8080/api/login/oauth2/code/kakao?code=${code}`
+        );
+        const authToken = res.data.authorization;
+        console.log(res);
 
-                // 응답 데이터 확인
-                const tokenBody = JSON.parse(localStorage.getItem('token_body'));
-                
-                if (tokenBody && tokenBody.status === 404 && tokenBody.message === "해당하는 정보의 사용자를 찾을 수 없습니다." && tokenBody.code === 2003) {
-                    // 추가 정보 입력 페이지로 리다이렉트
-                    navigate('/additionalInfo'); // '/additional-info'가 추가 정보 페이지의 라우트라고 가정
-                } else {
-                    window.location.href = "/";
-                }
-            } catch (error) {
-                console.error("카카오 로그인 중 오류 발생:", error);
-            }
-        };
+        localStorage.setItem("Authorization", authToken);
+        localStorage.setItem("token_body", JSON.stringify(res.data));
 
-        kakaoLogin();
-    }, [props.history, navigate]);
+        // 응답 데이터 확인
+        const tokenBody = JSON.parse(localStorage.getItem("token_body"));
 
-    return (
-        <div>로그인 중입니다. 잠시만 기다려주세요.</div>
-    );
-}
+        if (
+          tokenBody &&
+          tokenBody.status === 404 &&
+          tokenBody.message === "해당하는 정보의 사용자를 찾을 수 없습니다." &&
+          tokenBody.code === 2003
+        ) {
+          // 추가 정보 입력 페이지로 리다이렉트
+          navigate("/additionalInfo"); // '/additional-info'가 추가 정보 페이지의 라우트라고 가정
+        } else {
+          window.location.href = "/";
+        }
+      } catch (error) {
+        console.error("카카오 로그인 중 오류 발생:", error);
+      }
+    };
+
+    kakaoLogin();
+  }, [props.history, navigate]);
+
+  return <div>로그인 중입니다. 잠시만 기다려주세요.</div>;
+};
 
 export default KakaoAuthHandle;
