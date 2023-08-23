40787377f3266d96e6c012d21bb7cc1849f94c8a
minjae Park <cfctor9@gmail.com>
2023-08-23 16:45:17 +0900

    fix : login 문제

--- src/pages/NewsPage/NewsList.js
+++ src/pages/NewsPage/NewsList.js
@@ -1,37 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import News from '../../component/News';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import News from "../../component/News";
+import styled from "styled-components";
+import instance from "../oauth/instance";
 
 const NewsListContainer = styled.div`
-    width: 375px;
-    max-height: 812px;
-    box-sizing: border-box;
-    overflow: auto;
-    margin-left: auto;
-    margin-right: auto;
+  width: 375px;
+  max-height: 812px;
+  box-sizing: border-box;
+  overflow: auto;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 const NewsList = () => {
-    
-    const [newsArray,setNewsArray] = useState([])
-    useEffect(()=>{
-        axios.get("http://localhost:8080/news").then((response)=>{
-        // console.log(response.data) 
-        setNewsArray(response.data);
-    })
-    },[])
-    return (
-        <NewsListContainer>
-            {
-                newsArray.map(e => {
-                    return(
-                        <News key={e.news_index} news_imageUrl={e.news_imageUrl} news_title={e.news_title} news_content={e.news_content} news_index={e.news_index} />
-                    )
-                })
-            }
-        </NewsListContainer>
-    );
+  const [newsArray, setNewsArray] = useState([]);
+  useEffect(() => {
+    instance.get("http://localhost:8080/news").then((response) => {
+      console.log(response.data);
+      setNewsArray(response.data);
+    });
+  }, []);
+  return (
+    <NewsListContainer>
+      {newsArray.map((e) => {
+        return (
+          <News
+            key={e.news_index}
+            news_imageUrl={e.news_imageUrl}
+            news_title={e.news_title}
+            news_content={e.news_content}
+            news_index={e.news_index}
+          />
+        );
+      })}
+    </NewsListContainer>
+  );
 };
 
 export default NewsList;
