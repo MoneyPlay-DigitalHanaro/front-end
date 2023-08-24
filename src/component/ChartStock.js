b9898a310f3e3379c97de46c3f47aa1023fbc91d
YEJIN325 <seoyj0325@naver.com>
2023-08-24 17:40:35 +0900

    Fix: 주식 매수 페이지 UI 수정

--- src/component/ChartStock.js
+++ src/component/ChartStock.js
@@ -1,29 +1,31 @@
 import React from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid } from "recharts";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from "recharts";
 
 const ChartStock = ({ data }) => {
   return (
-    <LineChart
-      width={310}
-      height={220}
-      data={data.reverse()} // 데이터의 날짜 순서를 오름차순으로 변경
-      margin={{
-        top: 5,
-        right: 5,
-        left: 5,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="stockOpenDate" tick={false} />
-      <YAxis domain={["auto", "auto"]} tick={false} />
-      <Line
-        type="monotone"
-        dataKey="stockClosePrice"
-        stroke="#8884d8"
-        dot={false}
-      />
-    </LineChart>
+    <ResponsiveContainer width={330} height={270}>
+      <LineChart
+        width={310}
+        height={220}
+        data={data.reverse()} // 데이터의 날짜 순서를 오름차순으로 변경
+        margin={{
+          top: 5,
+          right: 5,
+          left: 5,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="stockOpenDate" tick={false} height={0}/>
+        <YAxis domain={["auto", "auto"]} tick={false} width={0}/>
+        <Line
+          type="monotone"
+          dataKey="stockClosePrice"
+          stroke="#8884d8"
+          dot={false}
+        />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
