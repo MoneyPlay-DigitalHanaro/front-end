07214e5e458cc66097ab0ebd783a315878563ccc
YEJIN325 <seoyj0325@naver.com>
2023-08-25 10:40:52 +0900

    Fix: axios import 제거

--- src/pages/wordQuiz/WordQuiz.js
+++ src/pages/wordQuiz/WordQuiz.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import instance from '../oauth/instance';
 import styled from 'styled-components';
 import Word from '../../component/Word';
